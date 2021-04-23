e3370f3f5e0461ad8ed13d847d08d803f117f0e6
Lee <Lee@DESKTOP-GBD3J1H>
2021-04-23 23:51:01 +0900

    add Prompt class

---
+++ src/Calendar/Prompt.java
@@ -0,0 +1,38 @@
+package Calendar;
+
+import java.util.Scanner;
+
+public class Prompt {
+
+	private final static String PROMPT = "cal> ";
+
+	public void runPrompt() {
+		Scanner in = new Scanner(System.in);
+		CalendarMain cal = new CalendarMain();
+
+		int year=0;
+		int month=0;
+
+		while (true) {
+			System.out.println("년을 입력하세요");
+			System.out.println("YEAR>");
+			year = in.nextInt();
+			System.out.println("달을 입력하세요");
+			System.out.println("MONTH>");
+			month = in.nextInt();
+			if (month == -1) {
+				break;
+			} else if (month > 12) {
+				continue;
+			}
+			cal.printClendar(year, month);
+		}
+		System.out.println("Bye~");
+		in.close();
+	}
+
+	public static void main(String[] args) {
+		Prompt p = new Prompt();
+		p.runPrompt();
+	}
+}
